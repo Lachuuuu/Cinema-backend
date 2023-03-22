50cd630116ed2e60bbc9cf25d6964f40034d3a74
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-03-22 01:06:48 +0100

    basic classes for halls

---
+++ src/main/java/com/Cinema/hall/Hall.java
@@ -0,0 +1,28 @@
+package com.Cinema.hall;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "hall")
+@JsonIdentityInfo(
+      generator = ObjectIdGenerators.PropertyGenerator.class,
+      property = "id")
+public class Hall {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id", nullable = false)
+   private Long id;
+   private String seatsMap;
+}
---
+++ src/main/java/com/Cinema/hall/HallRepository.java
@@ -0,0 +1,9 @@
+package com.Cinema.hall;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HallRepository extends JpaRepository<Hall, Long> {
+
+}
